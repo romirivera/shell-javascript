8f022b8fa5be2ad4880e334f294f1210341c0e00
Romina Rivera <rdrivera@uc.cl>
2025-03-23 23:47:30 -0300

    primera configuración

--- app/main.js
+++ app/main.js
@@ -1,11 +1,6 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-// Uncomment this block to pass the first stage
-// rl.question("$ ", (answer) => {
-//   rl.close();
-// });
