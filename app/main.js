64382da0d666bed56b37477994ab21ff37451aca
Romina Rivera <rdrivera@uc.cl>
2025-03-23 23:55:32 -0300

    [comentarios y salida en consola]

--- app/main.js
+++ app/main.js
@@ -1,3 +1,5 @@
+//Este código en Node.js usa el módulo readline para crear una interfaz de entrada y salida en la consola.
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -5,5 +7,9 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 rl.question('$ ', (answer) => {
+  console.log(`Ingresaste: ${answer}`);
   rl.close();
 });
+
+//Crea una interfaz de lectura/escritura en la terminal usando process.stdin para la entrada y process.stdout para la salida.
+//Muestra un prompt ($ ) en la terminal y espera la entrada del usuario. Cuando el usuario ingresa algo y presiona Enter, la función de callback recibe la entrada (answer). Luego, la interfaz se cierra con rl.close().
